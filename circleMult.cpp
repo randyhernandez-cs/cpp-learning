9db027371084403196e92af1955f588784b2fddb
Randy Hernandez <randyhernandez1020@gmail.com>
2026-04-25 14:52:34 -0700

    Add files via upload

---
+++ circleMult.cpp
@@ -0,0 +1,12 @@
+
+#include "circlefunctions.h"
+
+double CircleDiameter(double Rad) {
+	return Rad * 2;
+}
+double CircleArea(double Radius, double pi) {
+	return Radius * Radius * pi;
+}
+double CircleCircumference(double Radius, double pi) {
+	return  Radius * pi * 2;
+}
---
+++ circlefunctions.h
@@ -0,0 +1,7 @@
+
+#ifndef CONVERTER_H
+#define CONVERTER_H
+double CircleDiameter(double Radius);
+double CircleArea(double Radius, double pi);
+double CircleCircumference(double Radius, double pi);
+#endif
