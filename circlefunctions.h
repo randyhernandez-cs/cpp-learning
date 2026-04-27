420e4a49be5b63e83cdc013bcc5a9109ff51b38b
Randy Hernandez <randyhernandez1020@gmail.com>
2026-04-26 18:06:21 -0700

    Delete old circlefunctions.h
    -Replaced circlefunctions.h to snake_case convention
    -Renamed Functions to use camelCase
    -See STYLE_GUIDE.md for full conventions

--- circlefunctions.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef CONVERTER_H
-#define CONVERTER_H
-double CircleDiameter(double Radius);
-double CircleArea(double Radius, double pi);
-double CircleCircumference(double Radius, double pi);
-#endif
