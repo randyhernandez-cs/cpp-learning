030c21f840e155be533cead68e273d13af495ce1
Randy Hernandez <randyhernandez1020@gmail.com>
2026-06-20 08:01:06 -0700

    grade classifier with if-else chain fixed

--- grade_classifier/grade_project.cpp
+++ grade_classifier/grade_project.cpp
@@ -6,15 +6,14 @@ int main()
 	std::cin >> Grade;
 	if (Grade < 100 && Grade >= 90)
 		std::cout << "You got a A!" << '\n';
-	else if (Grade >= 89 && Grade <= 80)
+	else if (Grade <= 89 && Grade >= 80)
 		std::cout << "You got a B!" << '\n';
-	else if (Grade >= 79 && Grade <= 70)
+	else if (Grade <= 79 && Grade >= 70)
 		std::cout << "You got a C!" << '\n';
-	else if (Grade >= 69 && Grade <= 60)
+	else if (Grade <= 69 && Grade >= 60)
 		std::cout << "You got a D!" << '\n';
 	else
 		std::cout << "You got an F!" << '\n';
-
 	return 0;
 
 }
