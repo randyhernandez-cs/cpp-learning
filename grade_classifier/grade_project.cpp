83cdf082f0998cd97a6cbd169b72a7facfc677b0
Randy Hernandez <randyhernandez1020@gmail.com>
2026-06-19 19:45:49 -0700

    WIP: grade classifier with if-else chain, needs operator fix

--- grade_classifier/grade_project.cpp
+++ grade_classifier/grade_project.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 int main()
 {
-	
+	unsigned int Grade{};
+	std::cout << "Please enter grade in number form to convert to letter form:" << '\n';
+	std::cin >> Grade;
+	if (Grade < 100 && Grade >= 90)
+		std::cout << "You got a A!" << '\n';
+	else if (Grade >= 89 && Grade <= 80)
+		std::cout << "You got a B!" << '\n';
+	else if (Grade >= 79 && Grade <= 70)
+		std::cout << "You got a C!" << '\n';
+	else if (Grade >= 69 && Grade <= 60)
+		std::cout << "You got a D!" << '\n';
+	else
+		std::cout << "You got an F!" << '\n';
+
+	return 0;
+
 }
