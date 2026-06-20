917a2e8bf19b1cc8e751fb534af074823dc3387b
Randy Hernandez <randyhernandez1020@gmail.com>
2026-06-20 10:00:09 -0700

    Grade classifier - Bool value added, needs operator fix

--- grade_classifier/grade_project.cpp
+++ grade_classifier/grade_project.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+bool GradePorF(int Grade) {
+	if (Grade >= 70 && Grade <= 100)
+		std::cout << "You Passed!" << '\n';
+	else
+		std::cout << "You failed!" << '\n';
+	return 0;
+}
+
+
 int main()
 {
+	
 	unsigned int Grade{};
 	std::cout << "Please enter grade in number form to convert to letter form:" << '\n';
 	std::cin >> Grade;
+	GradePorF(Grade);
 	if (Grade < 100 && Grade >= 90)
 		std::cout << "You got a A!" << '\n';
 	else if (Grade <= 89 && Grade >= 80)
