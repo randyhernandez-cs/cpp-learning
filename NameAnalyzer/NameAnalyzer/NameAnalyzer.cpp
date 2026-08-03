ac8ecafff0a96c265521dfa8027ef554eb3e7fa4
Randy Hernandez <randyhernandez1020@gmail.com>
2026-08-03 15:02:02 -0700

    add function to count uppercase letters in name

--- NameAnalyzer/NameAnalyzer/NameAnalyzer.cpp
+++ NameAnalyzer/NameAnalyzer/NameAnalyzer.cpp
@@ -7,6 +7,7 @@ int main()
     std::cout << "Please enter your full name for analyization: ";
     std::string name{};
     std::getline(std::cin >> std::ws, name);    // reads full line, discards leading whitespace
+    std::cout << "You have a total of " << countCapitilization(name) << " capitals!" << '\n';   // counts capital letters in name
     countCharacters(name);  // displays character count of the entered name
     findCharacters(name);   // extracts and displays initials
 }
--- NameAnalyzer/NameAnalyzer/name_analyzer.cpp
+++ NameAnalyzer/NameAnalyzer/name_analyzer.cpp
@@ -1,7 +1,6 @@
 #include "name_analyzer.h"
 #include <iostream>
 #include <string>
-
 void countCharacters(const std::string& name) 
 {
 	std::cout << name << " has " << name.length() << " characters" << '\n'; // Prints the name and its character count
@@ -12,3 +11,15 @@ void findCharacters(const std::string& name)
 	std::cout << "The First Initial is: " << name[0] << '\n';
 	std::cout << "The Second Initial is: " << name[spacePosition + 1] << '\n';
 } 
+int countCapitilization(const std::string& name)
+{
+	int x{ 0 };
+	for (int i{ 0 }; i < name.length(); ++i)
+	{
+		if (name[i] >= 'A' && name[i] <= 'Z')
+		{
+			x += 1;
+		}
+	}
+	return x;
+}
