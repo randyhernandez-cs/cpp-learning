880818427fd9a47f2b8b3979960c5607d7f299ef
Randy Hernandez <randyhernandez1020@gmail.com>
2026-08-08 23:39:22 -0700

    Add upgraded circle calculator to portfolio

---
+++ circle_calculator/circle_functions.h
@@ -0,0 +1,6 @@
+#ifndef CONVERTER_FUNCTIONS_H
+#define CONVERTER_FUNCTIONS_H
+double circleDiameter(double radius);
+double circleArea(double radius, double pi);
+double circleCircumference(double radius, double pi);
+#endif
