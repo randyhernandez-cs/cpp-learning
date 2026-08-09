d0b66819b4779823cf399aaa7d3167278e6b8bc2
Randy Hernandez <randyhernandez1020@gmail.com>
2026-08-08 23:53:15 -0700

    Delete circle_calculator directory
    Remove old circle calculator files; replaced with proper project structure

--- circle_calculator/circle_functions.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONVERTER_FUNCTIONS_H
-#define CONVERTER_FUNCTIONS_H
-double circleDiameter(double radius);
-double circleArea(double radius, double pi);
-double circleCircumference(double radius, double pi);
-#endif
