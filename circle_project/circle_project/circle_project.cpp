e08d0659c3f47bde82d3ad401f19ca12a0f7cc50
Randy Hernandez <randyhernandez1020@gmail.com>
2026-08-10 21:17:04 -0700

    Add retry loop to circle calculator

--- circle_project/circle_project/circle_project.cpp
+++ circle_project/circle_project/circle_project.cpp
@@ -1,13 +1,21 @@
 #include "circle_functions.h"
-
 #include <iostream>
 
 int main()
 {
-    double radius{};
-    constexpr double pi{ 3.14159 };
-    std::cout << "Give me your radius to find the circumfrence, diameter, and area of your circle" << '\n' << "Radius: ";
-    std::cin >> radius;
-    std::cout << "Your values are:" << '\n' << "Diameter: " << circleDiameter(radius) << '\n' << "Area: " << circleArea(radius, pi) << '\n' << "Circumference: " << circleCircumference(radius, pi);
-    return 0;
+    char retry{};
+    do
+    {
+        double radius{};
+        constexpr double pi{ 3.14159 };
+        std::cout << "Give me your radius to find the circumfrence, diameter, and area of your circle" << '\n' << "Radius: ";
+        std::cin >> radius;
+        std::cout << "Your values are:" << '\n' << "Diameter: " << circleDiameter(radius) << '\n' << "Area: " << circleArea(radius, pi) << '\n' << "Circumference: " << circleCircumference(radius, pi);
+        std::cout << "Do you want to try a different number? (y/n): " << '\n';
+        std::cin >> retry;
+    }
+        while (retry == 'Y' || retry == 'y');
+    {
+
+    }
 }
