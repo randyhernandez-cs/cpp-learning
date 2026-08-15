5c1e191553fab4936a816724b94389946b631c31
Randy Hernandez <randyhernandez1020@gmail.com>
2026-08-14 22:56:26 -0700

    Add pointer and reference practice to sandbox pt 1

--- sandbox/sandbox/sandbox.cpp
+++ sandbox/sandbox/sandbox.cpp
@@ -5,6 +5,16 @@
 
 int main()
 {
-    std::cout << "Sandbox ready.\n";
-    return 0;
+	int x{ 10 };
+	std::cout << x << '\n';
+	std::cout << "the value of x is " << x << '\n';
+	int& y{ x };
+	std::cout << "input integer for y " << '\n';
+	std::cin >> y;
+	std::cout << "the value of x is " << x << '\n';
+	std::cout << "y is referenced to x, and thus y changes as well since y is technically x " << '\n';
+	int* z{&y};
+	std::cout << "z is pointed at y's address, changing z will change the address of y, which is references to x " << '\n';
+	*z = {3};
+	std::cout << "the value of x is " << x << '\n';
 }
