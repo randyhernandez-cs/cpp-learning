d4ede8ba2ec6ce9ea9e2c4c67305ff3b096c837e
Randy Hernandez <randyhernandez1020@gmail.com>
2026-08-17 14:59:25 -0700

    Expand pointer and reference practice in sandbox

--- sandbox/sandbox/sandbox.cpp
+++ sandbox/sandbox/sandbox.cpp
@@ -6,15 +6,19 @@
 int main()
 {
 	int x{ 10 };
-	std::cout << x << '\n';
 	std::cout << "the value of x is " << x << '\n';
+	std::cout << "x also has an address of " << &x << '\n';
 	int& y{ x };
 	std::cout << "input integer for y " << '\n';
 	std::cin >> y;
 	std::cout << "the value of x is " << x << '\n';
-	std::cout << "y is referenced to x, and thus y changes as well since y is technically x " << '\n';
+	std::cout << "x has an address of " << &x << '\n';
+	std::cout << "y also has an address of " << &y << '\n';
+	std::cout << "y is referenced to x and thus y shares an address with x, both are the same int " << '\n';
 	int* z{&y};
-	std::cout << "z is pointed at y's address, changing z will change the address of y, which is references to x " << '\n';
-	*z = {3};
+	std::cout << "z holds the address of x, a pointer holds an address, z can dereference, telling it to go to the address, potentially change, view " << '\n';
+	std::cout << "z has the stored address of " << z << '\n';
+	std::cout << "input integer to dereference z " << '\n';
+	std::cin >> *z;
 	std::cout << "the value of x is " << x << '\n';
 }
