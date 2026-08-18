8c2698b75af8f88c3caac38251907a696829b141
Randy Hernandez <randyhernandez1020@gmail.com>
2026-08-18 16:57:33 -0700

    Complete pointer and reference practice in sandbox

--- sandbox/sandbox/sandbox.cpp
+++ sandbox/sandbox/sandbox.cpp
@@ -16,9 +16,18 @@ int main()
 	std::cout << "y also has an address of " << &y << '\n';
 	std::cout << "y is referenced to x and thus y shares an address with x, both are the same int " << '\n';
 	int* z{&y};
-	std::cout << "z holds the address of x, a pointer holds an address, z can dereference, telling it to go to the address, potentially change, view " << '\n';
+	std::cout << "zz stores an address, and *z dereferences z to access the object at that address " << '\n';
 	std::cout << "z has the stored address of " << z << '\n';
 	std::cout << "input integer to dereference z " << '\n';
 	std::cin >> *z;
 	std::cout << "the value of x is " << x << '\n';
+	int c{ 5 };
+	std::cout << "We create c with the address of " << &c << " and a value of "<< c << '\n';
+	std::cout << "With a new int, we can change where z points at" << '\n';
+	z = &c ;
+	std::cout << "since z is now pointed at c, z holds the address of " << z << '\n';
+	std::cout << "enter an integer to dereference z" << '\n';
+	std::cin >> *z;
+	std::cout << "The value of c is " << c << '\n';
+	std::cout << "The value of x remains unchanged: " << x << '\n';
 }
