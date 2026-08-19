f0007a86c98485b59131218378c654be051ae5f5
Randy Hernandez <randyhernandez1020@gmail.com>
2026-08-19 13:29:29 -0700

    Refactor functions to operate on player struct

--- sandbox/sandbox/sandbox.cpp
+++ sandbox/sandbox/sandbox.cpp
@@ -5,27 +5,41 @@
 struct player
 {
 	std::string name{};
-	int health{};
+	int totalHealth{};
 	int level{};
 	bool alive{};
 };
 
-int health(int& health)
+void health(player& Cin)
 {
 	int damage{};
 	std::cout << "please enter how much damage you want to take" << '\n';
 	std::cin >> damage;
-	health = health - damage;
-	return health;
+	Cin.totalHealth = Cin.totalHealth - damage;
+}
+
+void fatality(player& Cin)
+{
+	if (Cin.totalHealth <= 0)
+	{
+		Cin.totalHealth = 0;
+		std::cout << "Your character is dead!" << '\n';
+		Cin.alive = false;
+	}
+	else 
+	{
+		std::cout << "Your character is alive!" << '\n';
+		Cin.alive = true;
+	}
 }
 
 int main()
 {
 	player Cin{ "Cin", 100, 10, true  };
 	std::cout << "The name of this character is: " << Cin.name << '\n';
-	std::cout << "The health of this character is: " << Cin.health << '\n';
+	std::cout << "The health of this character is: " << Cin.totalHealth << '\n';
 	std::cout << "The level of this character is: " << Cin.level << '\n';
-	std::cout << "Alive of this character is: " << Cin.alive << '\n';
-	health(Cin.health);
-	std::cout << "Cin has " << Cin.health << " health" << '\n';
+	health(Cin);
+	fatality(Cin);
+	std::cout << "Cin has " << Cin.totalHealth << " health" << '\n';
 }
