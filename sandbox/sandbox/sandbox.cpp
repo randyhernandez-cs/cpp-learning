132a20a33e74218f69186141850af03db4934dd6
Randy Hernandez <randyhernandez1020@gmail.com>
2026-08-20 10:09:19 -0700

    Complete struct and pointer practice in sandbox

--- sandbox/sandbox/sandbox.cpp
+++ sandbox/sandbox/sandbox.cpp
@@ -10,36 +10,56 @@ struct player
 	bool alive{};
 };
 
-void health(player& Cin)
+void health(player& Character)
 {
 	int damage{};
 	std::cout << "please enter how much damage you want to take" << '\n';
 	std::cin >> damage;
-	Cin.totalHealth = Cin.totalHealth - damage;
+	Character.totalHealth = Character.totalHealth - damage;
 }
 
-void fatality(player& Cin)
+void fatality(player& Character)
 {
-	if (Cin.totalHealth <= 0)
+	if (Character.totalHealth <= 0)
 	{
-		Cin.totalHealth = 0;
+		Character.totalHealth = 0;
 		std::cout << "Your character is dead!" << '\n';
-		Cin.alive = false;
+		Character.alive = false;
 	}
 	else 
 	{
 		std::cout << "Your character is alive!" << '\n';
-		Cin.alive = true;
+		Character.alive = true;
 	}
+}
+void displayCharacter(const player& Character)
+{
+	std::cout << "The address of the character struct is: " << &Character << '\n';
+	std::cout << "The name of this character is: " << Character.name << '\n';
+	std::cout << "The health of this character is: " << Character.totalHealth << '\n';
+	std::cout << "The level of this character is: " << Character.level << '\n';
+}
+void pointCharacter(player* &x)
+{
+	std::cout << x << '\n';
+	std::cout << x->alive << '\n';
+	std::cout << x->name << '\n';
+	std::cout << x->totalHealth << '\n';
+	x->level = 2;
 }
 
 int main()
 {
-	player Cin{ "Cin", 100, 10, true  };
-	std::cout << "The name of this character is: " << Cin.name << '\n';
-	std::cout << "The health of this character is: " << Cin.totalHealth << '\n';
-	std::cout << "The level of this character is: " << Cin.level << '\n';
-	health(Cin);
-	fatality(Cin);
-	std::cout << "Cin has " << Cin.totalHealth << " health" << '\n';
+	player* x{ nullptr };
+	player Character{ "", 100, 1, true  };
+	std::cout << "Create the name for this character: ";
+	std::cin >> Character.name;
+	displayCharacter(Character);
+	health(Character);
+	fatality(Character);
+	std::cout << Character.name << " has " << Character.totalHealth << " health" << '\n';
+	x = { &Character };
+	pointCharacter(x);
+	displayCharacter(Character);
+	fatality(Character);
 }
