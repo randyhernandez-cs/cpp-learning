eae9c207c66c46a70621b4afb8f96cc1f1c637b0
Randy Hernandez <randyhernandez1020@gmail.com>
2026-08-26 07:05:58 -0700

    Complete player roster vector practice

--- sandbox/sandbox/sandbox.cpp
+++ sandbox/sandbox/sandbox.cpp
@@ -2,26 +2,27 @@
 #include <iostream>
 #include <string>
 #include <vector>
-struct player
+struct Player
 {
 	std::string name{ "Character" };
 	int health{100};
 	int level{1};
 };
-void displayPlayers(const std::vector<player>& x)
+void displayPlayers(const std::vector<Player>& x)
 {
 	for (int y{}; y < x.size(); ++y)
 	{
-		std::cout << x.at(y).name << '\n';
-		std::cout << x.at(y).health << '\n';
-		std::cout << x.at(y).level << '\n';
+		std::cout << "This index number is: " << y << '\n';
+		std::cout << "Their name is: " << x.at(y).name << '\n';
+		std::cout << "Their health is: " << x.at(y).health << '\n';
+		std::cout << "Their level is: " << x.at(y).level << '\n';
 	}
 }
 int main()
 {
-	player A;
-	player B;
-	player C;
-	std::vector characters{A, B, C};
+	Player alice{"Alice", 30, 6};
+	Player bob{"Bob", 100, 56};
+	Player charlie{"Charlie", 77, 100};
+	std::vector characters{alice, bob, charlie};
 	displayPlayers(characters);
 }
