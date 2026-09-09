d217f9802bbba05f9aa56ed1cc70ff10cbca32f6
Randy Hernandez <randyhernandez1020@gmail.com>
2026-09-09 10:56:49 -0700

    Moved personal reading tracker to correct file, updated personal reading files

---
+++ practice/personal_reading_tracker/personal_reading_tracker/personal_reading_tracker.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <array>
+#include "reader_tracker_header.h"
+
+int main()
+{
+   
+}
---
+++ practice/personal_reading_tracker/personal_reading_tracker/reader_tracker_header.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <array>
+
+struct book 
+{
+	std::string title{};
+	int readingProgress{};
+};
+std::vector<book>
